4b274411275fac8ee7b4f56d5b9f3c8325d138f0
Jevin <jevinthk007@gmail.com>
2024-04-07 12:06:17 -0400

    codename change

--- 101-kittens-carousel-static-website-ec2/Solutions/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/Solutions/userdata.sh
@@ -1,12 +1,29 @@
-#! /bin/bash
+#!/bin/bash
+
+# update the OS
 yum update -y
+
+#install Apache
 yum install httpd -y
+
+# copy content to /var/www/html folder
+cd /var/www/html
+FOLDER="https://raw.githubusercontent.com/Jevin2225/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+wget ${FOLDER}/cat3.png
+
+# start and enable Apache service
+systemctl start httpd
+systemctl enable httpd
 
 
 
 
 dnf install httpd -y
-FOLDER="https://raw.githubusercontent.com/awsdevopsteam/101-cfn-static-website-ec2/master/static-web"
+
 cd /var/www/html
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
